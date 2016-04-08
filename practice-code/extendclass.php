fd09370d50e70a68baefc2ecbde620617ee7d1f7
vanessahenson <vanessa@voltagead.com>
2016-04-08 08:00:40 -0600

    added the angular weather and time app

---
+++ practice-code/extendclass.php
@@ -0,0 +1,17 @@
+<?php
+	
+
+?>
+
+use extends 
+
+ex: class Dog extends Animal
+
+** the construct method doesn't come with 
+you make a new construct and then assign it the parents construct
+
+public function __construct() {
+	parent::__construct('yourinfo')
+}
+
+you can extend multiple classes
